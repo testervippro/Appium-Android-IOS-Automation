bdcc658533357b7761b946b1ea0597957c6a4913
cuxuanthoai <cuxuanthoai@gmail.com>
2025-01-04 00:45:13 +0700

    add log ios  capcity .sh

---
+++ appium-automation/src/main/java/com/thoaikx/AppiumServerManager.java
@@ -0,0 +1,7 @@
+package com.thoaikx;
+
+public class AppiumServerManager {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
--- ios_appium_config.sh
+++ ios_appium_config.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # Automatically retrieve values for desired capabilities
+# Set the app path dynamically
+app="/Users/mac/Documents/Appium-Android-IOS-Automation/appium-automation/src/test/resources/iosapp/My Demo App.app"
+
 
 # Get the device name (for simulators)
 deviceName=$(xcrun simctl list devices | grep Booted | awk -F'[()]' '{print $1}' | xargs)
@@ -14,20 +17,9 @@ platformName="iOS"
 # Get the platform version of the booted simulator
 platformVersion=$(xcrun simctl list devices | grep Booted | awk -F'[()]' '{print $2}' | xargs | cut -d'.' -f1-2)
 
-# Set the app path dynamically
-app="/Users/mac/Documents/Appium-Android-IOS-Automation/appium-automation/src/test/resources/iosapp/My Demo App.app"
 
 # Set the automation name (always XCUITest for iOS)
 automationName="XCUITest"
-
-# Set noReset to true (modify as needed)
-noReset=true
-
-# Set the updated WebDriverAgent bundle ID (optional)
-updatedWDABundleId="com.yourcompany.WebDriverAgentRunner"
-
-# Set useNewWDA to true (modify as needed)
-useNewWDA=true
 
 # Generate the JSON
 cat <<EOF
@@ -38,8 +30,6 @@ cat <<EOF
   "platformVersion": "$platformVersion",
   "app": "$app",
   "automationName": "$automationName",
-  "noReset": $noReset,
-  "updatedWDABundleId": "$updatedWDABundleId",
-  "useNewWDA": $useNewWDA
+  "appium:showXcodeLog": true
 }
 EOF
