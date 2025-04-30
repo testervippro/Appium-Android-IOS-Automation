96a7e0c9f4c7b92cabf797afc913b755a4fc2ac7
cuxuanthoai <cuxuanthoai@gmail.com>
2025-04-30 14:12:07 +0700

    add utils IOSSimulatorUltis.java

--- src/main/java/com/dan/utils/IOSSimulatorUltis.java
+++ src/main/java/com/dan/utils/IOSSimulatorUltis.java
@@ -7,7 +7,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Iterator;
 
-public class IOSSimulatorHelper {
+public class IOSSimulatorUltis {
 
     // Nested class to hold device information
     public static class DeviceInfo {
