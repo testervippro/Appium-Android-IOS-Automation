bbefa2ffb8d3cc31dd5c947e0a3a9eeec19c4299
cuxuanthoai <cuxuanthoai@gmail.com>
2025-05-01 11:07:30 +0700

    add modifies BaseTest

--- src/test/java/com/dan/base/BaseTest.java
+++ src/test/java/com/dan/base/BaseTest.java
@@ -10,6 +10,7 @@ import io.qameta.allure.Step;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.time.Duration;
 
@@ -18,49 +19,18 @@ public class BaseTest {
     protected AppiumDriver driver = null;
     protected WebDriverWait wait;
 
-//    AppiumServiceBuilder builder = new AppiumServiceBuilder()
-//            .withIPAddress("127.0.0.1")
-//            .usingPort(0);
-//
-//    AppiumDriverLocalService service = AppiumDriverLocalService.buildService(builder);
-//    public int usedPort = service.getUrl().getPort();
-    private AppiumDriverLocalService service = AppiumDriverLocalService.buildDefaultService();
-    @BeforeClass(alwaysRun = true)
+    @BeforeTest(alwaysRun = true)
     @Parameters({"platform", "udid", "platformVersion"})
-    public void preCondition(@Optional("Android") String platform, @Optional("emulator-5554") String udid, @Optional("") String platformVersion) throws MalformedURLException {
-         service.start();
+    public void preCondition(@Optional("Android") String platform, @Optional("emulator-5554") String udid, @Optional("") String platformVersion) throws IOException {
         driver = new DriverFactory().createInstance(platform, udid, platformVersion);
         wait = new WebDriverWait(driver, Duration.ofSeconds(60));
     }
 
-    @AfterClass(alwaysRun = true)
+    @AfterTest(alwaysRun = true)
     public void tearDown() throws InterruptedException {
         if (driver != null) {
             driver.quit();
         }
-       service.stop();
-//        Thread.sleep(1000);
-//        stopAppiumService(service);
     }
 
-
-
-    public static void stopAppiumService(AppiumDriverLocalService service) {
-        if (service != null) {
-            try {
-                if (service.isRunning()) {
-                    service.stop();
-
-                    // Additional cleanup for assurance
-                    if (System.getProperty("os.name").toLowerCase().contains("win")) {
-                        Runtime.getRuntime().exec("taskkill /F /IM node.exe");
-                    } else {
-                        Runtime.getRuntime().exec("pkill -f appium");
-                    }
-                }
-            } catch (Exception e) {
-                System.err.println("Error stopping Appium: " + e.getMessage());
-            }
-        }
-    }
 }
