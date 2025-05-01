128a38f31dd12ea138eca2a862129737b30bddb9
cuxuanthoai <cuxuanthoai@gmail.com>
2025-05-01 11:43:26 +0700

    modified app.android.appPackage,IOSSimulatorUtils.java

--- src/main/java/com/dan/utils/IOSSimulatorUtils.java
+++ src/main/java/com/dan/utils/IOSSimulatorUtils.java
@@ -7,7 +7,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Iterator;
 
-public class IOSSimulatorUltis {
+public class IOSSimulatorUtils {
 
     // Nested class to hold device information
     public static class DeviceInfo {
